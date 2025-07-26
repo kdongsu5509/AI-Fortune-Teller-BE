5c4b0ae998d3a17107c24a44aab0f39067fdd3e6
dongsu_ko <127917642+kdongsu5509@users.noreply.github.com>
2025-07-26 16:44:51 +0900

    init/ migrate prev vcs to this
    기존 프로젝트의 코드 복잡도가 큰 관계로 새롭게 레포지토리를 생성 후 관리하기 위해 새로운 레포지토리를 형성하였습니다.
    prev repo : https://github.com/kdongsu5509/fortune-teller.git

---
+++ src/main/java/com/example/ai_fortunre_teller/AiFortunreTellerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ai_fortunre_teller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFortunreTellerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFortunreTellerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ai_fortunre_teller/AiFortunreTellerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ai_fortunre_teller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFortunreTellerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
